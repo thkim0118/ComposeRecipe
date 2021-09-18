df99da9b4b6cf761737dbf4522df7e1ede9fcbcc
Terry <choir0118@gmail.com>
2021-09-19 01:00:47 +0900

    feat : Default modifier component

--- app/src/main/java/com/terry/composerecipe/ui/MainActivity.kt
+++ app/src/main/java/com/terry/composerecipe/ui/MainActivity.kt
@@ -3,16 +3,20 @@ package com.terry.composerecipe.ui
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,8 +38,15 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun PhotographerCard() {
-    Row {
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = { /* Ignoring onClick */ })
+            .padding(16.dp)
+    ) {
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
